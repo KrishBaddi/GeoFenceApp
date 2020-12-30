c678864a100fd972b36ea461242167798e13a4f4
Ramkrishna <Ramkrishna@kaodim.com>
2020-12-30 14:17:57 +0800

    Initial Commit

---
+++ iOS-Geofence-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Geofence-Demo
+//
+//  Created by Kaodim MacMini on 30/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
